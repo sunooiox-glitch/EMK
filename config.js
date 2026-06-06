6864d1e041c51073008bda34c16d8599fa4c2ebe
sunooiox-glitch <sunooiox@gmail.com>
2026-06-06 07:29:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212786946506;
+global.pairingNumber = 33759850405;
 global.owner = [
   ['33759850405', 'KIM SUN OO', true],
   ['', 'Owner 2', true],
